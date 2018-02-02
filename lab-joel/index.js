e9275c6989da7cac394c866ff1da48eb17420c72
Joel Clair <clair.joel@yahoo.com>
2018-02-02 09:41:29 -0800

    crud tests working

--- lab-joel/index.js
+++ lab-joel/index.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-if(process.env.NODE_ENV !== 'production') require('dotenv').config()
+if(process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const server = require('./lib/server')
-server.start(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
+const server = require('./lib/server');
+server.start(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`));
--- lab-joel/model/note.js
+++ lab-joel/model/note.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
-const uuid = require('uuid/v4')
+const uuid = require('uuid/v4');
 
 module.exports = function Note(title, content) {
   return new Promise((resolve, reject) => {
-    if(!title || !content) return reject(new Error('Validation Error. Cannot create Note. Title and Content required.'))
-    this._id = uuid()
-    this.title = title
-    this.content = content
+    if(!title || !content) return reject(new Error('Validation Error. Cannot create Note. Title and Content required.'));
+    this._id = uuid();
+    this.title = title;
+    this.content = content;
 
-    return resolve(this)
-  })
-}
+    return resolve(this);
+  });
+};
